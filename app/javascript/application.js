2e5b53dadcdea78cc9f309b95bbd38a4d0743a4f
ishmaelakaboa <ishmaelakaboa@gmail.com>
2023-01-26 11:42:40 +0000

    install bootstrap javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "custom/jquery.min.js"
+import "custom"
+import "custom/sticky-navbar.js"
+import "custom/gulpfile.js"
+
